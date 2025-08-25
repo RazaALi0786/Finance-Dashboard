5114ede208919014556569de86533ba0eb0a9091
RazaALi0786 <razaali26296@gmail.com>
2025-08-26 01:44:50 +0530

    Initial Ui starting

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,5 +1,32 @@
 import { Button } from "@/components/ui/button";
-
+import { UserButton } from "@clerk/nextjs";
 export default function Home() {
-  return <p>This is an authenticated route</p>;
+  return <UserButton afterSignOutUrl="/" />;
 }
+
+// import {
+//   SignedIn,
+//   SignedOut,
+//   RedirectToSignIn,
+//   UserButton,
+// } from "@clerk/nextjs";
+
+// export default function DashboardPage() {
+//   return (
+//     <>
+//       <SignedIn>
+//         <div className="p-6">
+//           <div className="flex justify-between items-center">
+//             <h1 className="text-2xl font-bold">Dashboard</h1>
+//             <UserButton afterSignOutUrl="/" />
+//           </div>
+//           <p className="mt-4">Hello, welcome to your dashboard 🚀</p>
+//         </div>
+//       </SignedIn>
+
+//       <SignedOut>
+//         <RedirectToSignIn />
+//       </SignedOut>
+//     </>
+//   );
+// }
